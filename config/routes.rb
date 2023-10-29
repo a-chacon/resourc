0109bd0c2dca443158fbed816cedefb07439fc86
a-chacon <andres.ch@protonmail.com>
2023-10-29 13:13:14 -0300

    feat: new look

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,13 @@
 Rails.application.routes.draw do
   scope '(:locale)', locale: /en|es/ do
+    root 'links#index'
     get 'tag_suggestions', to: 'tags#suggestions'
     get 'open_graph', to: 'links#open_graph'
-    resources :users, except: ['create']
-    root 'links#index'
+    resources :users, except: %w[create index]
     get 'auth/:provider/callback', to: 'sessions#create'
     get 'sessions/sign_out', to: 'sessions#destroy'
     # resources :sessions, only: %w[new create]
-    resources :links
+    resources :links, only: %i[index new create]
 
     get 'terms', to: 'application#terms'
     get 'privacy', to: 'application#privacy'
