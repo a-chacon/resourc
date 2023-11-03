7b713904103fb44ab385490d4dd762ebc19df7b0
a-chacon <andres.ch@protonmail.com>
2023-11-02 21:18:45 -0300

    fix: wrap tags

--- app/controllers/links_controller.rb
+++ app/controllers/links_controller.rb
@@ -15,7 +15,7 @@ class LinksController < ApplicationController
                        .limit(20)
 
     @q = Link.ransack(params[:q])
-    @pagy, @records = pagy(@q.result(distinct: true).includes(:user, :tags))
+    @pagy, @records = pagy(@q.result(distinct: true).order(created_at: :desc).includes(:user, :tags))
   end
 
   # GET /links/new
