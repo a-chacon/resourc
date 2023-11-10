04cac9ab927d785c32d63afb43f27c7b9fe1f53d
a-chacon <andres.ch@protonmail.com>
2023-11-10 09:41:22 -0300

    feat: implement hashblur

--- app/javascript/controllers/application.js
+++ app/javascript/controllers/application.js
@@ -1,9 +1,12 @@
 import { Application } from "@hotwired/stimulus"
+import { BlurhashController } from "stimulus-blurhash";
 
 const application = Application.start()
 
 // Configure Stimulus development experience
 application.debug = false
 window.Stimulus   = application
+
+Stimulus.register("blurhash", BlurhashController);
 
 export { application }
